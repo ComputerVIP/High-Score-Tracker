fddccf4f5afe5c11c062567c7eb4afa681be77c6
Ferriswheel490 <fairus.delacruz@ucas-edu.net>
2025-02-27 13:03:40 -0700

    heh

--- display_scores_and_profiles.py
+++ display_scores_and_profiles.py
@@ -1,23 +1,19 @@
 
 import csv
 
-def profile_and_scores():
-    ans = input("What is your name?\n")
-    if ans == True:
 
-        with open("Scores.csv", "r") as file:
-            reader = csv.reader(file)
-            for row in reader:
-                if ans.upper() in str(row).upper():
-                    ask = input("What is the password for this profile?\n")
-                    if ask.upper() in row[0].upper():
-                        print("Name: ",row[1])
-                        print("\nHard reaction test score: ",row[2])
-                        print("Easy reaction test score: ",row[3])
-                        print("Number guess score: ",row[4])
-                        print("\nProfile: ",row[5])
-                else:
-                    pass
-    else:
-        print("Profile doesn't exsit")
-        return profile_and_scores()
+ans = input("What is your name?\n")
+
+with open("Scores.csv", "r") as file:
+    reader = csv.reader(file)
+    for row in reader:
+        if ans.upper() in str(row).upper():
+            ask = input("What is the password for this profile?\n")
+            if ask.upper() in row[0].upper():
+                print("Name: ",row[1])
+                print("\nHard reaction test score: ",row[2])
+                print("Easy reaction test score: ",row[3])
+                print("Number guess score: ",row[4])
+                print("\nProfile: ",row[5])
+        else:
+            pass
