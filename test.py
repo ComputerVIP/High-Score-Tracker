cab0a74c7d7b4690e369c12e5bae8f2792b5ba3b
ComputerVIP <vincent.johnson@ucas-edu.net>
2025-02-27 13:01:09 -0700

    Fixed indentation

--- test.py
+++ test.py
@@ -5,10 +5,10 @@ with open("Scores.csv", "r") as file:
   next(file)
   users = []
   for row in reader:
-      users.append([row[1], row[2]])
+    users.append([row[1], row[2]])
   sorted_users = []
   for user in users:
-      sorted_users.append(user[1] + " " + user[0])
+    sorted_users.append(user[1] + " " + user[0])
   sorted_users = sorted(sorted_users, reverse=True)
   for user in sorted_users:
     name, score = user.split(" ")
