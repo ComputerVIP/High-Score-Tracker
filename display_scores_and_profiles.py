1f43f124a55a983309f1699cd93d153aeeacfc42
ComputerVIP <vincent.johnson@ucas-edu.net>
2025-02-27 18:43:00 -0700

    Fixed two minor issues

--- display_scores_and_profiles.py
+++ display_scores_and_profiles.py
@@ -8,12 +8,10 @@ def print_profiles():
         reader = csv.reader(file)
         for row in reader:
             if ans.upper() in str(row[1]).upper():
-                ask = input("What is the password for this profile?\n")
-                if ask.upper() in row[0].upper():
-                    print("Name: ",row[1])
-                    print("\nHard reaction test score: ",row[2])
-                    print("Easy reaction test score: ",row[3])
-                    print("Number guess score: ",row[4])
-                    print("\nProfile: ",row[5])
+                print("Name: ",row[1])
+                print("\nHard reaction test score: ",row[2])
+                print("Easy reaction test score: ",row[3])
+                print("Number guess score: ",row[4])
+                print("\nProfile: ",row[5])
             else:
                 pass
