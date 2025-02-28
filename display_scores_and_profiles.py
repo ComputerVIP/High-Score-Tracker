2075de188c9afe46239603e362f3b0db44a6406d
Ferriswheel490 <fairus.delacruz@ucas-edu.net>
2025-02-27 21:09:36 -0700

    did something

--- display_scores_and_profiles.py
+++ display_scores_and_profiles.py
@@ -2,16 +2,22 @@
 import csv
 
 def print_profiles():
-    ans = input("What is your name?\n")
+    while True:
+        ans = input("What is your name?\n")
+        found = False
 
-    with open("Scores.csv", "r") as file:
-        reader = csv.reader(file)
-        for row in reader:
-            if ans.upper() in str(row[1]).upper():
-                print("Name: ",row[1])
-                print("\nHard reaction test score: ",row[2])
-                print("Easy reaction test score: ",row[3])
-                print("Number guess score: ",row[4])
-                print("\nProfile: ",row[5])
-            else:
-                pass
+        with open("Scores.csv", "r") as file:
+            reader = csv.reader(file)
+            for row in reader:
+                if ans.upper() in str(row[1]).upper():
+                    print("Name: ",row[1])
+                    print("\nHard reaction test score: ",row[2])
+                    print("Easy reaction test score: ",row[3])
+                    print("Number guess score: ",row[4])
+                    print("\nProfile: ",row[5])
+                    found = True
+                    break
+        if not found:
+            print("This profile doesn't exist please try again\n")  
+
+print_profiles()
